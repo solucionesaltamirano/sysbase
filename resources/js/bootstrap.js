88d9a3979e397196e705813c9a427f3be4911d9c
gersonaltamirano <gersonaltamirano@gmail.com>
2021-01-13 11:51:34 -0600

    Instalación y conf pack Laravel Websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,19 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    wsHost:            window.location.hostname,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    wsPort: 6001,
+    wwsPort: 6002,
+    // forceTLS: true,
+    encrypted:         true,
+    enabledTransports: ['ws', 'wss'],
+});
